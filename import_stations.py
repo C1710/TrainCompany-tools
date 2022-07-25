f701a11f7e8aa55aef61a77da6b07ca8cd24f06b
Constantin A <git-user@c1710.de>
2022-07-25 20:08:49 +0200

    Add GPX export CLI option

--- import_stations.py
+++ import_stations.py
@@ -10,7 +10,7 @@ from typing import List, Tuple
 from cli_utils import check_files, process_station_input
 from geo.location_data import add_location_data_to_list
 from structures import DataSet
-from structures.station import iter_stations_by_codes_reverse
+from structures.station import iter_stations_by_codes_reverse, Station
 from tc_utils import TcFile
 from tc_utils.stations import add_stations_to_file
 
@@ -21,7 +21,8 @@ def import_stations_into_tc(station_codes: List[str],
                             override_stations: bool = False,
                             update_stations: bool = False,
                             append: bool = False,
-                            trassenfinder: bool = False
+                            trassenfinder: bool = False,
+                            gpx: bool = False
                             ) -> TcFile:
     data_set = DataSet.load_data(data_directory)
     station_codes = process_station_input(station_codes, data_set)
@@ -47,6 +48,8 @@ def import_stations_into_tc(station_codes: List[str],
 
     if trassenfinder:
         create_trassenfinder([(code, code_to_station[code.upper()].name) for code in station_codes], tc_directory)
+    if gpx:
+        create_gpx(stations, tc_directory)
 
     return station_json
 
@@ -65,22 +68,8 @@ def create_trassenfinder(stations: List[Tuple[str, str]], tc_directory: PathLike
         outfile.writelines(lines)
 
 
-def import_regex_stations_into_tc(stations_regex: str,
-                                  tc_directory: PathLike | str = '..',
-                                  data_directory: PathLike | str = 'data',
-                                  override_stations: bool = False,
-                                  update_stations: bool = False,
-                                  append: bool = False
-                                  ) -> TcFile:
-    regex = re.compile(stations_regex, re.RegexFlag.IGNORECASE)
-    data_set = DataSet.load_data(data_directory)
-    stations = [station for station in data_set.station_data if any((regex.match(code) for code in station.codes))]
-
-    add_location_data_to_list(stations)
-
-    station_json = TcFile('Station', tc_directory)
-    add_stations_to_file(stations, station_json, override_stations, update_stations, append=append)
-    return station_json
+def create_gpx(stations: List[Station], tc_directory: PathLike | str = '..'):
+    pass
 
 
 if __name__ == '__main__':
@@ -88,13 +77,8 @@ if __name__ == '__main__':
     script_dir = os.path.dirname(script_path)
 
     parser = argparse.ArgumentParser(description='Importiere neue Betriebsstellen in TrainCompany')
-    regex_or_ril100_list = parser.add_mutually_exclusive_group()
-    regex_or_ril100_list.add_argument('--stations', metavar='RIL100', type=str, nargs='+',
-                                      help='Die RIL100-Codes, die hinzugefügt werden sollen')
-    regex_or_ril100_list.add_argument('--regex', type=str, metavar='REGEX',
-                                      help="Ein regulärer Ausdruck, "
-                                           "nach dem die Betriebsstellen hinzugefügt werden sollen.")
-    regex_or_ril100_list.required = True
+    parser.add_argument('--stations', metavar='RIL100', type=str, nargs='+', required=True,
+                        help='Die RIL100-Codes, die hinzugefügt werden sollen')
     parser.add_argument('--tc-dir', dest='tc_directory', metavar='VERZEICHNIS', type=str,
                         default=os.path.dirname(script_dir),
                         help="Das Verzeichnis, in dem sich die TrainCompany-Daten befinden")
@@ -110,27 +94,19 @@ if __name__ == '__main__':
                         help="Fügt alles am Ende ein")
     parser.add_argument('--trassenfinder', action='store_true',
                         help="Legt eine Trassenfinder-ähnliche Datei mit den Haltestellen an.")
+    parser.add_argument('--gpx', action='store_true',
+                        help="Legt eine GPX-Datei mit Wegpunkten an, die bspw. auf brouter.de importiert werden kann.")
     args = parser.parse_args()
 
     check_files(args.tc_directory, args.data_directory)
-    if args.stations is not None:
-        station_json = import_stations_into_tc(
-            args.stations,
-            args.tc_directory,
-            args.data_directory,
-            args.override_stations,
-            args.update_stations,
-            append=args.append,
-            trassenfinder=args.trassenfinder
-        )
-    else:
-        # We (should?) have a regex
-        station_json = import_regex_stations_into_tc(
-            args.regex,
-            args.tc_directory,
-            args.data_directory,
-            args.override_stations,
-            args.update_stations,
-            append=args.append
-        )
+    station_json = import_stations_into_tc(
+        args.stations,
+        args.tc_directory,
+        args.data_directory,
+        args.override_stations,
+        args.update_stations,
+        append=args.append,
+        trassenfinder=args.trassenfinder,
+        gpx=args.gpx
+    )
     station_json.save()
