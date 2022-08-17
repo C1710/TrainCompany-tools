73ae8df172df2a4a4aaedfd93b03b5e2e4d91197
Constantin A <git-user@c1710.de>
2022-08-17 18:34:17 +0200

    Refactoring

--- geo/__init__.py
+++ geo/__init__.py
@@ -12,15 +12,10 @@ default_projection_version = 1
 crs_wgs84 = pyproj.CRS('WGS84')
 crs_proj = pyproj.CRS('epsg:3035')
 crs_robinson = pyproj.CRS('esri:54030')
+
 transformer = pyproj.Transformer.from_crs(crs_wgs84, crs_proj)
-transformer_reverse = pyproj.Transformer.from_crs(crs_proj, crs_wgs84)
 transformer_robinson = pyproj.Transformer.from_crs(crs_wgs84, crs_robinson)
 projection = pyproj.Proj('epsg:3035')
-
-origin_x_tc: float = 6.482451
-origin_y_tc: float = 51.766433
-scale_x_tc: float = 625.0 / (11.082989 - origin_x_tc)
-scale_y_tc: float = 385.0 / (49.445616 - origin_y_tc)
 
 
 @dataclass(frozen=True)
@@ -50,37 +45,42 @@ class Location:
             )
         return location
 
-    @staticmethod
-    def from_projection(x: int, y: int, version: int = default_projection_version) -> Location:
+    @classmethod
+    def from_projection(cls, x: int, y: int, version: int = default_projection_version) -> Location:
         if version == 0:
             return Location.from_tc(x, y)
         if version == 1:
-            origin_x, origin_y, scale_x, scale_y = get_origin_scale(
-                lambda lon, lat: projection(longitude=lon, latitude=lat, errcheck=True))
-            y = -y
-            x /= scale_x
-            y /= scale_y
-            x += origin_x
-            y += origin_y
-            longitude, latitude = projection(x, y, inverse=True)
+            return cls.from_projection_with_fun(
+                projection_fun=lambda lon, lat: projection(longitude=lon, latitude=lat, errcheck=True),
+                projection_fun_reverse=lambda x, y: projection(x, y, inverse=True, errcheck=True),
+                x=x, y=y)
         elif version == 2:
-            origin_x, origin_y, scale_x, scale_y = get_origin_scale(
-                lambda lon, lat: transformer.transform(xx=lon, yy=lat, errcheck=True))
-            y = -y
-            x /= scale_x
-            y /= scale_y
-            x += origin_x
-            y += origin_y
-            longitude, latitude = transformer_reverse.transform(x, y)
+            return cls.from_projection_with_fun(
+                projection_fun=lambda lon, lat: transformer.transform(xx=lon, yy=lat, errcheck=True),
+                projection_fun_reverse=lambda x, y: transformer.transform(xx=x, yy=y,
+                                                                          direction=TransformDirection.INVERSE,
+                                                                          errcheck=True),
+                x=x, y=y)
         elif version == 3:
-            origin_x, origin_y, scale_x, scale_y = get_origin_scale(
-                lambda lon, lat: transformer.transform(yy=lon, xx=lat, errcheck=True))
-            y = -y
-            x /= scale_x
-            y /= scale_y
-            x += origin_x
-            y += origin_y
-            longitude, latitude = transformer_robinson.transform(x, y, direction=TransformDirection.INVERSE)
+            # There seems to be a bug in the robinson transformer that longitude and latitude are swapped
+            return cls.from_projection_with_fun(
+                projection_fun=lambda lon, lat: transformer_robinson.transform(xx=lat, yy=lon, errcheck=True),
+                projection_fun_reverse=lambda x, y: transformer_robinson.transform(xx=y, yy=x,
+                                                                                   direction=TransformDirection.INVERSE,
+                                                                                   errcheck=True),
+                x=x, y=y)
+
+    @classmethod
+    def from_projection_with_fun(cls, projection_fun: Callable[[float, float], Tuple[float, float]],
+                                 projection_fun_reverse: Callable[[float, float], Tuple[float, float]],
+                                 x: float, y: float) -> Location:
+        origin_x, origin_y, scale_x, scale_y = get_origin_scale(projection_fun)
+        x /= scale_x
+        y = -y / scale_y
+        x += origin_x
+        y += origin_y
+        longitude, latitude = projection_fun_reverse(x, y)
+
         if abs(x - 0) < 2:
             assert abs(longitude - origin_x_tc) < 0.002, abs(longitude - origin_x_tc)
         if abs(y - 0) < 2:
@@ -91,9 +91,10 @@ class Location:
             assert abs(latitude - location_nn.latitude) < 0.002, abs(latitude - location_nn.latitude)
         assert -180.0 <= longitude <= 180.0
         assert -90.0 <= latitude <= 90.0
-        return Location(
-            latitude=latitude,
-            longitude=longitude
+
+        return cls(
+            longitude=longitude,
+            latitude=latitude
         )
 
     def to_projection(self, version: int = default_projection_version) -> Tuple[int, int]:
@@ -105,27 +106,25 @@ class Location:
             return self.to_tc()
         else:
             if version == 1:
-                x, y = projection(longitude=self.longitude, latitude=self.latitude, errcheck=True)
-                origin_x, origin_y, scale_x, scale_y = get_origin_scale(
+                return self.to_projection_with_fun(
                     lambda lon, lat: projection(longitude=lon, latitude=lat, errcheck=True))
             elif version == 2:
-                x, y = transformer.transform(xx=self.longitude, yy=self.latitude, errcheck=True)
-                origin_x, origin_y, scale_x, scale_y = get_origin_scale(
+                return self.to_projection_with_fun(
                     lambda lon, lat: transformer.transform(xx=lon, yy=lat, errcheck=True))
             elif version == 3:
-                try:
-                    x, y = transformer_robinson.transform(yy=self.longitude, xx=self.latitude, errcheck=True)
-                except Exception as e:
-                    print(self.longitude, self.latitude)
-                    raise e
-                origin_x, origin_y, scale_x, scale_y = get_origin_scale(
-                    lambda lon, lat: transformer_robinson.transform(yy=lon, xx=lat, errcheck=True))
+                # There seems to be a bug in the robinson transformer that longitude and latitude are swapped
+                return self.to_projection_with_fun(
+                    lambda lon, lat: transformer_robinson.transform(xx=lat, yy=lon, errcheck=True))
             else:
                 raise ValueError("Projection version is not supported")
-            x -= origin_x
-            y -= origin_y
-            # Note that we negate y here, as we want y to face southwards
-            return int(x * scale_x), int(-y * scale_y)
+
+    def to_projection_with_fun(self, projection_fun: Callable[[float, float], Tuple[float, float]]) -> Tuple[int, int]:
+        x, y = projection_fun(self.longitude, self.latitude)
+        origin_x, origin_y, scale_x, scale_y = get_origin_scale(projection_fun)
+        x -= origin_x
+        y -= origin_y
+        # Note that we negate y here, as we want y to face southwards
+        return int(x * scale_x), int(-y * scale_y)
 
     def distance(self, other: Location) -> int:
         return geopy.distance.geodesic(
@@ -147,18 +146,25 @@ location_kdn = Location(
     latitude=50.809494066048444,
     longitude=6.48224930984118
 )
-location_nn = Location(
-    latitude=49.444986777895615,
-    longitude=11.082370202236625
-)
+origin_x_tc: float = location_kdn.longitude
+
 location_ha = Location(
     latitude=51.76666898080754,
     longitude=8.943182001417538
 )
+origin_y_tc: float = location_ha.latitude
+
+location_nn = Location(
+    latitude=49.444986777895615,
+    longitude=11.082370202236625
+)
+scale_x_tc: float = 625.0 / (location_nn.longitude - location_kdn.longitude)
+scale_y_tc: float = 385.0 / (location_nn.latitude - location_ha.latitude)
 
 
 @lru_cache
-def get_origin_scale(projection_fun: Callable[[float, float], Tuple[float, float]]) -> Tuple[float, float, float, float]:
+def get_origin_scale(projection_fun: Callable[[float, float], Tuple[float, float]]) -> Tuple[
+    float, float, float, float]:
     """returns: origin_x, origin_y, scale_x, scale_y"""
     x_kdn, y_kdn = projection_fun(location_kdn.longitude, location_kdn.latitude)
     x_nn, y_nn = projection_fun(location_nn.longitude, location_nn.latitude)
